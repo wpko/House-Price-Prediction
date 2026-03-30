1a46d81059e737e6bc336839b9863b9ece18f2c7
wpko <waiphyoko168@gmail.com>
2026-03-31 04:19:18 +0700

    Initial commit - House Price Prediction APP

--- model/Predict_fun.py
+++
@@ -1,9 +0,0 @@
-import pandas as pd
-
-def predict_price(model_dict,input_data:dict):
-    model = model_dict["model"]
-    features = model_dict["features"]
-    
-    df = pd.DataFrame([input_data])
-    df = df[features]
-    return model.predict(df)[0]
